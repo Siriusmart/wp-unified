ef51489614ca4b4b8406e35f02bfaa80711fd2a4
Siriusmart <sirius@siri.ws>
2026-05-18 23:16:15 +0100

    rename function now public

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -2,6 +2,7 @@ import webpan = require("webpan");
 import type { ProcessorOutputRaw } from "webpan/dist/types/processorStates";
 import { Processor } from 'unified';
 import { VFile } from "vfile";
+export declare function runRename(expr: string, pathToProccess: string): string;
 interface UnifiedPluginResponse {
     pluginName: string;
     pluginOptions: any;
